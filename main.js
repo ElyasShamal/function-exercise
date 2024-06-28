2b0ee89eb4b1aae03a0cca64043d6b25d581d702
ElyasShamal <Elyas.shamal2@gmail.com>
2024-06-27 23:39:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,60 @@ return '|_(* ~ *)_|'
 }
 }
 console.log(toEmoticon("whatever")) 
+
+
+function tipCalculator(quality, total){
+  let tipPercentage;
+
+  switch(quality){
+    case "bad":
+    tipPercentage = 0.05;
+    break;
+    case "ok":
+    tipPercentage = 0.15;
+    break;
+    case 'good':
+      tipPercentage = 0.20;
+      break;
+    case 'excellent':
+      tipPercentage = 0.30;
+      break;
+    default:
+      tipPercentage = 0.18;
+  }
+
+  return total * tipPercentage
+
+}
+
+console.log(tipCalculator('bad', 100));       
+console.log(tipCalculator('ok', 100));         
+console.log(tipCalculator('good', 100));      
+console.log(tipCalculator('excellent', 100));  
+console.log(tipCalculator('average', 100));
+
+const whatRelation = percentSharedDNA => {
+    if (percentSharedDNA === 100) {
+        return 'You are likely identical twins.'
+    }
+    if (percentSharedDNA > 34) {
+        return 'You are likely parent and child or full siblings.'
+    }
+    if (percentSharedDNA > 13) {
+        return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+    }
+    if (percentSharedDNA > 5) {
+        return 'You are likely 1st cousins.'
+    }
+    if (percentSharedDNA > 2) {
+        return 'You are likely 2nd cousins.'
+    }
+    if (percentSharedDNA > 0) {
+        return 'You are likely 3rd cousins'
+    }
+    return 'You are likely not related.'
+}
+
+console.log(whatRelation(34))
+
+console.log(whatRelation(3))
